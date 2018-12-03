37f2dfbb55306a2cff10fbf6509b8e0299f9e2fa
Orealdsf <41176743+Orealdsf@users.noreply.github.com>
2018-12-03 20:36:15 +0800

    新增系網

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, abort
 
+
+
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -7,6 +9,10 @@ from linebot.exceptions import (
     InvalidSignatureError,LineBotApiError
 )
 from linebot.models import *
+
+#from bs4 import BeautifulSoup
+
+#from urllib.request import urlretrieve
 
 app = Flask(__name__)
 
@@ -18,7 +24,7 @@ handler = WebhookHandler('7b3daf73b5fe9358c1085d2cb8dd01d7')
 
 
 
-# 監聽所有來自 /callback 的 Post Request
+#  /callback  Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -33,67 +39,165 @@ def callback():
         abort(400)
     return 'OK'
 
-#追蹤
-@handler.add(FollowEvent)
-def handle_follow(event):
-    print("in Follow")
-    button_template_message =ButtonsTemplate(
-                                    thumbnail_image_url="https://i.imgur.com/eTldj2E.png?1",
-                                    title='Menu', 
-                                    text='歡迎follow',
-                                    image_size="cover",
-                                    actions=[
-                                        MessageTemplateAction(
-                                            label='功能1', text='function-1'
-                                        ),
-                                        MessageTemplateAction(
-                                            label='功能2', text='function-2'
-                                        ),
-                                        MessageTemplateAction(
-                                            label='功能3', text='function-3'
-                                        ),
-                                    ]
-                                )
-                                
-    line_bot_api.reply_message(
-        event.reply_token,
-        TemplateSendMessage(
-            alt_text="Follow Event",
-            template=button_template_message
+
+#def Dcard():
+#    url = 'https://www.dcard.tw/f'
+#    resp = requests.get(url)
+#    #HTTP 
+#    print(resp.status_code)
+#    #  Beautiful Soup 
+#    soup = BeautifulSoup(resp.text, 'html.parser')
+#    # print(soup.prettify())
+#    dcard_title = soup.find_all('h3', re.compile('PostEntry_title_'))
+
+#    for index, item in enumerate(dcard_title[:10]):
+#        print("{0:2d}. {1}".format(index + 1, item.text.strip()))
+
+def movie():
+    target_url = 'https://movies.yahoo.com.tw/'
+    rs = requests.session()
+    res = rs.get(target_url, verify=False)
+    res.encoding = 'utf-8'
+    soup = BeautifulSoup(res.text, 'html.parser')   
+    content = ""
+    for index, data in enumerate(soup.select('div.movielist_info h1 a')):
+        if index == 20:
+            return content       
+        title = data.text
+        link =  data['href']
+        content += '{}\n{}\n'.format(title, link)
+    return content
+
+def CS():           
+    message = TemplateSendMessage(
+            alt_text='Carousel template',
+            template=CarouselTemplate(
+                columns=[
+                    CarouselColumn(
+                        thumbnail_image_url='https://upload.wikimedia.org/wikipedia/zh/thumb/6/63/National_Pingtung_University_logo.svg/1200px-National_Pingtung_University_logo.svg.png',
+                        title='國立屏東大學_資訊科學系',
+                        text='本系致力於智慧雲端系統與應用開發之研發工作',
+                        actions=[
+                            PostbackTemplateAction(
+                                label='系所簡介',
+                                text='系所簡介',
+                                data='action=buy&itemid=1'
+                            ),
+                            MessageTemplateAction(
+                                label='教學師資',
+                                text='教學師資'
+                            ),
+                            MessageTemplateAction(
+                                label='研究所',
+                                text='研究所'
+                            ),
+                        ]
+                    ),
+                    CarouselColumn(
+                        thumbnail_image_url='https://upload.wikimedia.org/wikipedia/zh/thumb/6/63/National_Pingtung_University_logo.svg/1200px-National_Pingtung_University_logo.svg.png',
+                        title='國立屏東大學_資訊科學系',
+                        text='本系致力於智慧雲端系統與應用開發之研發工作',
+                        actions=[
+                            PostbackTemplateAction(
+                                label='實驗室',
+                                text='實驗室',
+                                data='action=buy&itemid=1'
+                            ),
+                            MessageTemplateAction(
+                                label='大學部',
+                                text='大學部'
+                            ),
+                            MessageTemplateAction(
+                                label='未來學生',
+                                text='未來學生'
+                            ),
+                        ]
+                    ),
+                ]
+            )
         )
+    return message
+
+def CSIntroduction():
+    message = TemplateSendMessage(
+    alt_text='Buttons template',
+    template=ButtonsTemplate(
+        thumbnail_image_url='https://example.com/image.jpg',
+        title='系所簡介',
+        text='你可以問我',
+        actions=[
+            PostbackTemplateAction(
+                label='系所介紹',
+                text='系所介紹',
+                data='action=buy&itemid=1'
+            ),
+            MessageTemplateAction(
+                label='發展特色',
+                text='發展特色'
+            ),
+            MessageTemplateAction(
+                label='未來發展',
+                text='未來發展'
+            )
+        ]
     )
-
-# 處理訊息
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    # 推送訊息
-    #line_bot_api.push_message('Ubc4902b9b76350b957769c5376e7f43c',TextSendMessage(text='我是基德 是名機器人'))
-
-    # 回應訊息
-
-    #圖片訊息
-    if event.message.text== '圖片' :
-        message = ImageSendMessage(
-        original_content_url='',
-        preview_image_url=''
-        )
-        line_bot_api.reply_message(event.reply_token, message)
+)
+    return message
+    
     
 
-    elif event.message.text == '==':
-        message = TextSendMessage(text='終於有人知道= =中間不要加空格')
-        line_bot_api.reply_message(event.reply_token,message)
 
-    #影片訊息
-    elif event.message.text =='影片':
-        message = VideoSendMessage(
-            original_content_url='https://example.com/original.mp4',
-            preview_image_url='https://example.com/preview.jpg'
-        )
+
+
+
+    
+    
+  
+
+# 
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    # 
+    #line_bot_api.push_message('Ubc4902b9b76350b957769c5376e7f43c',TextSendMessage(text=''))
+    if event.message.text == "CS": 
+        message=CS()
+        line_bot_api.reply_message(event.reply_token, message)
+    if event.message.text == "系所簡介": 
+        message = CSIntroduction()
+        line_bot_api.reply_message(event.reply_token, message)
+    if event.message.text == "教學師資": 
+        message = TextSendMessage(text='True')
+        line_bot_api.reply_message(event.reply_token, message)
+    if event.message.text == "研究所": 
+        message = TextSendMessage(text='True')
+        line_bot_api.reply_message(event.reply_token, message)
+    if event.message.text == "實驗室": 
+        message = TextSendMessage(text='True')
+        line_bot_api.reply_message(event.reply_token, message)
+    if event.message.text == "大學部": 
+        message = TextSendMessage(text='True')
+        line_bot_api.reply_message(event.reply_token, message)
+    if event.message.text == "未來學生": 
+        message = TextSendMessage(text='True')
         line_bot_api.reply_message(event.reply_token, message)
 
-    #位置訊息
-    elif event.message.text =='位置':
+
+    if event.message.text == "系所介紹": 
+        message = TextSendMessage(text='True')
+        line_bot_api.reply_message(event.reply_token, message)
+    if event.message.text == "發展特色": 
+        message = TextSendMessage(text='True')
+        line_bot_api.reply_message(event.reply_token, message)
+    if event.message.text == "未來發展": 
+        message = TextSendMessage(text='True')
+        line_bot_api.reply_message(event.reply_token, message)
+
+
+    
+    if event.message.text == '==':
+        message = TextSendMessage(text='fuck')
+        line_bot_api.reply_message(event.reply_token,message)
+    elif event.message.text =='location':
        message = LocationSendMessage(
            title='my location',
            address='Tokyo',
@@ -101,64 +205,17 @@ def handle_message(event):
            longitude=139.70372892916203
        )
        line_bot_api.reply_message(event.reply_token, message)
-
-    #組圖訊息
-    elif event.message.text == '組圖':
-        message = ImagemapSendMessage(
-            base_url='https://example.com/base',
-            alt_text='this is an imagemap',
-            base_size=BaseSize(height=1040, width=1040),
-            actions=[
-                URIImagemapAction(
-                    link_uri='https://example.com/',
-                    area=ImagemapArea(
-                        x=0, y=0, width=520, height=1040
-                    )
-                ),
-                MessageImagemapAction(
-                    text='hello',
-                    area=ImagemapArea(
-                        x=520, y=0, width=520, height=1040
-                    )
-                )
-           ]
-        )
-        line_bot_api.reply_message(event.reply_token, message)
-
-    #按鈕介面
-    #elif event.message.text =='按鈕介面訊息':
-    #    message = TemplateSendMessage(
-    #        alt_text='Buttons template',
-    #        template=ButtonsTemplate(
-    #            thumbnail_image_url='https://example.com/image.jpg',
-    #            title='Menu',
-    #            text='Please select',
-    #            actions=[
-    #                PostbackTemplateAction(
-    #                    label='postback',
-    #                    text='postback text',
-    #                    data='action=buy&itemid=1'
-    #                ),
-    #                MessageTemplateAction(
-    #                    label='message',
-    #                    text='message text'
-    #                ),
-    #               URITemplateAction(
-    #                    label='uri',
-    #                    uri='http://example.com/'
-    #                )
-    #           ]
-    #        )
-   # )
-   # line_bot_api.reply_message(event.reply_token, message)
-    
-
     else:
         message = TextSendMessage(text=event.message.text)
         line_bot_api.reply_message(event.reply_token, message)
 
-  
-  
+    
+
+    
+
+
+    
+
     
 
 import os
@@ -168,88 +225,4 @@ if __name__ == "__main__":
 
 
 
-#爬蟲（尚未確認功能，需測試回傳）
-#引入函式庫
-import requests 
-import json
-import datetime
-import re
 
-from selenium import webdriver
-from bs4 import BeautifulSoup
-from linebot import LineBotApi
-from linebot.models import TextSendMessage
-
-#dcard
-url = 'https://www.dcard.tw/f'
-resp = requests.get(url)
-#HTTP 要求是否已經被完成
-print(resp.status_code)
-# 以 Beautiful Soup 解析 HTML 程式碼
-soup = BeautifulSoup(resp.text, 'html.parser')
-# print(soup.prettify())
-dcard_title = soup.find_all('h3', re.compile('PostEntry_title_'))
-print('Dcard 熱門前十文章標題：')
-for index, item in enumerate(dcard_title[:10]):
-    print("{0:2d}. {1}".format(index + 1, item.text.strip()))
-
-
-# 下載 Yahoo 首頁內容
-r = requests.get('https://tw.yahoo.com/')
-
-# 確認是否下載成功
-if r.status_code == requests.codes.ok:
-  soup = BeautifulSoup(r.text, 'html.parser')
-
-  # 以 CSS 的 class 抓出各類頭條新聞
-  stories = soup.find_all('a', class_='story-title')
-  for s in stories:
-    # 新聞標題
-    print("標題：" + s.text)
-    # 新聞網址
-    print("網址：" + s.get('href'))
-
-
-# Google 搜尋 URL
-google_url = 'https://www.google.com.tw/search'
-
-# 查詢參數
-my_params = {'q': 'example'}
-
-# 下載 Google 搜尋結果
-r = requests.get(google_url, params = my_params)
-
-if r.status_code == requests.codes.ok:
-  soup = BeautifulSoup(r.text, 'html.parser')
-
-  # 觀察 HTML 原始碼
-  # print(soup.prettify())
-
-  # 以 CSS 的選擇器來抓取 Google 的搜尋結果
-  items = soup.select('div.g > h3.r > a[href^="/url"]')
-  for i in items:
-    # 標題
-    print("標題：" + i.text)
-    # 網址
-    print("網址：" + i.get('href'))    
-
-#氣象
-import pandas as pd
-import numpy as np
-from bs4 import BeautifulSoup
-import datetime
-import os
-import pyodbc
-
-# 抓現在時間
-today = str(datetime.date.today())
-cwb_data = "cwb_weather_data"
-if not os.path.exists(cwb_data):
-    os.mkdir(cwb_data)
-
-#連接現有氣象 api
-import urllib.request
-import zipfile 
-res ="http://opendata.cwb.gov.tw/opendataapi?dataid=F-D0047-093&authorizationkey=CWB-3FB0188A-5506-41BE-B42A-3785B42C3823"
-urllib.request.urlretrieve(res,"F-D0047-093.zip")
-f=zipfile.ZipFile('F-D0047-093.zip')
